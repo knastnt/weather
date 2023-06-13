31680e3c8b67484b60026d0d7bbe33b5ede103bf
knastnt <456kot@mail.ru>
2023-06-13 22:18:18 +1000

    lib

--- src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapMapper.java
+++ src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapMapper.java
@@ -1,12 +1,13 @@
 package ru.knastnt.weather.weatherparser.implementations.openweathermap;
 
+import com.github.prominence.openweathermap.api.model.forecast.Forecast;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 import ru.knastnt.weather.weatherparser.dtos.WeatherDto;
-import ru.knastnt.weather.weatherparser.implementations.openweathermap.dtos.OpenWeatherMapResponseDto;
 
-@Component
+@Mapper
 public class OpenWeatherMapMapper {
-    public WeatherDto map(OpenWeatherMapResponseDto openWeatherMapResponseDto) {
+    public WeatherDto map(Forecast forecast) {
         //todo
         return null;
     }
--- src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapParser.java
+++ src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapParser.java
@@ -1,16 +1,15 @@
 package ru.knastnt.weather.weatherparser.implementations.openweathermap;
 
+import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
+import com.github.prominence.openweathermap.api.enums.Language;
+import com.github.prominence.openweathermap.api.enums.UnitSystem;
+import com.github.prominence.openweathermap.api.model.forecast.Forecast;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import ru.knastnt.weather.weatherparser.CityNotFoundException;
 import ru.knastnt.weather.weatherparser.dtos.WeatherDto;
 import ru.knastnt.weather.weatherparser.implementations.WeatherParser;
-import ru.knastnt.weather.weatherparser.implementations.openweathermap.dtos.OpenWeatherMapResponseDto;
 
 @Slf4j
 @Component
@@ -23,46 +22,17 @@ public class OpenWeatherMapParser implements WeatherParser {
 
     @Override
     public WeatherDto parseForCity(String city) {
-        String url = new StringBuilder()
-                .append("https://api.openweathermap.org/data/2.5/weather?q=")
-                .append(city)
-                .append("&lang=RU")
-                .append("&units=metric")
-                .append("&appid=")
-                .append(apiKey)
-                .toString();
+        OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(apiKey);
 
-        try {
-            ResponseEntity<OpenWeatherMapResponseDto> result = new RestTemplate()
-                    .exchange(url, HttpMethod.GET, null, OpenWeatherMapResponseDto.class);
-            log.debug("Result of calling REST: status: {}, result body: {}", result.getStatusCode(), result.getBody());
-            return mapper.map(result.getBody());
-        } catch (Exception e) {
-            if (e.getMessage().contains("city not found")){
-                throw new CityNotFoundException();
-            }
-            log.error("Exception in REST calling", e);
-            throw e;
-        }
+        final Forecast forecast = openWeatherClient
+                .forecast5Day3HourStep()
+                .byCityName(city)
+                .language(Language.RUSSIAN)
+                .unitSystem(UnitSystem.METRIC)
+                .count(15)
+                .retrieve()
+                .asJava();
 
-
-        /*URLConnection connection = new URL(url).openConnection();
-        connection.setRequestProperty("Accept-Charset", "UTF-8");
-        InputStream response = connection.getInputStream();
-
-        try (Scanner scanner = new Scanner(response)) {
-            String responseBody = scanner.useDelimiter("\\A").next(); // считываем ответ сервера
-            JSONObject jsonObject = new JSONObject(responseBody);
-            JSONObject main = jsonObject.getJSONObject("main");
-            double temperature = main.getDouble("temp"); // температура в Кельвинах
-            int pressure = main.getInt("pressure"); // атмосферное давление
-            int humidity = main.getInt("humidity"); // влажность
-            JSONObject wind = jsonObject.getJSONObject("wind");
-            double windSpeed = wind.getDouble("speed"); // скорость ветра
-            JSONArray weatherArray = jsonObject.getJSONArray("weather");
-            JSONObject weather = weatherArray.getJSONObject(0);
-            String description = weather.getString("description"); // описание погоды
-            System.out.println("Weather in " + city + ": " + description + ", Temperature: " + (temperature - 273.15) + " °C, Pressure: " + pressure + " hPa, Humidity: " + humidity + " %, Wind speed: " + windSpeed + " m/s");
-        }*/
+        return mapper.map(forecast);
     }
 }
