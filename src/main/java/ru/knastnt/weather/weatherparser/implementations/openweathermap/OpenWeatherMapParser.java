1c7967f9dbb62273df7d2179430b87c1b1296629
knastnt <456kot@mail.ru>
2023-06-14 00:43:13 +1000

    fix exception throwing

--- src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapParser.java
+++ src/main/java/ru/knastnt/weather/weatherparser/implementations/openweathermap/OpenWeatherMapParser.java
@@ -3,12 +3,14 @@ package ru.knastnt.weather.weatherparser.implementations.openweathermap;
 import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
 import com.github.prominence.openweathermap.api.enums.Language;
 import com.github.prominence.openweathermap.api.enums.UnitSystem;
+import com.github.prominence.openweathermap.api.exception.NoDataFoundException;
 import com.github.prominence.openweathermap.api.model.forecast.Forecast;
 import com.github.prominence.openweathermap.api.model.weather.Weather;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.knastnt.weather.weatherparser.CityNotFoundException;
 import ru.knastnt.weather.weatherparser.dtos.WeatherDto;
 import ru.knastnt.weather.weatherparser.implementations.WeatherParser;
 
@@ -23,25 +25,31 @@ public class OpenWeatherMapParser implements WeatherParser {
 
     @Override
     public WeatherDto parseForCity(String city) {
-        OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(apiKey);
+        try {
+            OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(apiKey);
 
-        Weather weather = openWeatherClient
-                .currentWeather()
-                .single()
-                .byCityName(city)
-                .language(Language.RUSSIAN)
-                .unitSystem(UnitSystem.METRIC)
-                .retrieve()
-                .asJava();
-        Forecast forecast = openWeatherClient
-                .forecast5Day3HourStep()
-                .byCityName(city)
-                .language(Language.RUSSIAN)
-                .unitSystem(UnitSystem.METRIC)
-                .count(15)
-                .retrieve()
-                .asJava();
+            Weather weather = openWeatherClient
+                    .currentWeather()
+                    .single()
+                    .byCityName(city)
+                    .language(Language.RUSSIAN)
+                    .unitSystem(UnitSystem.METRIC)
+                    .retrieve()
+                    .asJava();
+            Forecast forecast = openWeatherClient
+                    .forecast5Day3HourStep()
+                    .byCityName(city)
+                    .language(Language.RUSSIAN)
+                    .unitSystem(UnitSystem.METRIC)
+                    .count(15)
+                    .retrieve()
+                    .asJava();
 
-        return mapper.map(weather, forecast);
+            return mapper.map(weather, forecast);
+
+        } catch (NoDataFoundException e) {
+            if (e.getMessage() != null && e.getMessage().contains("Please, check requested location.")) throw new CityNotFoundException();
+            throw e;
+        }
     }
 }
