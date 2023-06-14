4e5e24779d53ac4b69b09affd905fe3a551133cd
knastnt <456kot@mail.ru>
2023-06-14 10:13:38 +1000

    remove unused

--- src/main/java/ru/knastnt/weather/doc/JasperPdfCreator.java
+++ src/main/java/ru/knastnt/weather/doc/JasperPdfCreator.java
@@ -1,24 +1,18 @@
 package ru.knastnt.weather.doc;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.SneakyThrows;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
-//import net.sf.jasperreports.export.exporter.PdfExporter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JasperPdfCreator {
     @SneakyThrows
@@ -26,7 +20,7 @@ public class JasperPdfCreator {
         createPdf();
     }
 
-    public static File createPdf() throws JRException, SQLException, IOException {
+    public static void createPdf() throws JRException, IOException {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("CITY", "city_город");
         parameters.put("COUNTRY", "страна_country");
@@ -34,27 +28,11 @@ public class JasperPdfCreator {
         URL resource = JasperPdfCreator.class.getClassLoader().getResource("templates/weather.jrxml");
         String s = JasperCompileManager.compileReportToFile(resource.getFile());
 
-//        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(Arrays.asList(
-//                new ExampleData("WEATHER", "WEATHER норм"),
-//                new ExampleData("baz", "qux")));
-
         InputStream resourceAsStream = JasperPdfCreator.class.getClassLoader().getResourceAsStream("templates/weather1.jasper");
         JasperPrint jasperPrint = JasperFillManager.fillReport(resourceAsStream, parameters, getDataSource());
 
         byte[] bytes = JasperExportManager.exportReportToPdf(jasperPrint);
         Files.write(Path.of("e:/example.pdf"), bytes);
-
-        /*OutputStream outputStream = new FileOutputStream(pdfFile);
-
-        PdfExporter exporter = new PdfExporter();
-        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
-        SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
-        exporter.setConfiguration(configuration);
-
-        exporter.exportReport();*/
-
-        return null;
     }
 
     private static JRDataSource getDataSource() {
@@ -85,23 +63,5 @@ public class JasperPdfCreator {
             res.setF2("value" + counter.incrementAndGet());
             return res;
         }
-    }
-}
-
-class ExampleData {
-    private final String field1;
-    private final String field2;
-
-    public ExampleData(String field1, String field2) {
-        this.field1 = field1;
-        this.field2 = field2;
-    }
-
-    public String getField1() {
-        return field1;
-    }
-
-    public String getField2() {
-        return field2;
     }
 }
